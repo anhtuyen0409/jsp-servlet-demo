1ea4bc1ecb79fef3d60b1ee17bafdbf0fcfcc49b
Tuyen <nguyenanhtuyen10a5@gmail.com>
2023-07-07 19:25:01 +0700

    first commit

---
+++ src/main/java/com/nguyenanhtuyen/demo/Demo.java
@@ -0,0 +1,41 @@
+package com.nguyenanhtuyen.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Demo")
+public class Demo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public Demo() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+
+		PrintWriter out = response.getWriter();
+
+		String name = request.getParameter("name");
+		out.println(name);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+
+		PrintWriter out = response.getWriter();
+
+		String name = request.getParameter("name");
+		out.println(name);
+	}
+
+}
